6ae7baf6eb8b38e390f6ac69dcb37869ab0e9d34
Dmitriy Dudnik <wed2288@gmail.com>
2021-10-24 00:11:52 +0300

    JS для галереи

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
 
+const galleryItemsMarkup = galleryItems
+  .map(
+    (item) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" target="_self" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
+
+galleryEl.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+
+  const instance = basicLightbox
+    .create(
+      `
+  <div class="modal">
+  <img src="${event.target.dataset.source}" class="js-modal-img" width="800" height="600">
+  </div>
+`,
+      {
+        onShow: (instance) => {
+          window.addEventListener("keydown", onKeyboardClick);
+          function onKeyboardClick(event) {
+            if (event.code === "Escape") {
+              instance.close();
+              window.removeEventListener("keydown", onKeyboardClick);
+            }
+          }
+
+          instance
+            .element()
+            .querySelector(".js-modal-img")
+            .addEventListener("click", () => {
+              instance.close();
+            });
+        },
+      }
+    )
+    .show();
+}
 console.log(galleryItems);
